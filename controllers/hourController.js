1d2b5bb36929eafb9c4ba5bd32e52f9a3a163244
elsilv <elsi.silvennoinen@gmail.com>
2021-07-11 16:27:24 +0300

    Made DELETE /api/workingHours/:id to work

--- controllers/hourController.js
+++ controllers/hourController.js
@@ -22,7 +22,7 @@ exports.getWorkingHours = async (req, res, next) => {
 exports.addWorkingHours = async (req, res, next) => {
     try {
         const { text, amount } = req.body;
-        
+
         const workingHour = await WorkingHour.create(req.body);
         return res.status(201).json({
             success: true,
@@ -36,7 +36,30 @@ exports.addWorkingHours = async (req, res, next) => {
     }
 }
 
-// @route DELETE /api/workingHours/:id
+// DELETE /api/workingHours/:id
 exports.deleteWorkingHours = async (req, res, next) => {
-    res.status('DELETE workingHours');
+    try {
+        const workingHour = await WorkingHour.findById(req.params.id);
+
+        if(workingHour) {
+            await workingHour.remove();
+            return res.status(200).json({
+                success: true
+            })
+        }
+
+        else { 
+            res.status(404).json({
+            success: false,
+            error: 'No found'
+            });
+        }
+        
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: 'Server error'
+        });
+    }
 }
