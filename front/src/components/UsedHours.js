32618456d1a0dde44dcffe0449107f22050f3429
elsilv <elsi.silvennoinen@gmail.com>
2021-07-12 13:19:15 +0300

    Changed delete buttons style

--- front/src/components/UsedHours.js
+++ front/src/components/UsedHours.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
+import * as TiIcons from 'react-icons/ti';
 
 export const UsedHours = (props) => {
     const { deleteHistory } = useContext(GlobalContext);
@@ -10,7 +11,7 @@ export const UsedHours = (props) => {
         <li className="minus">
                     {props.usedHours.text} <span>{amount}</span> 
                     <button onClick={() => deleteHistory(props.usedHours._id)}
-                    className="delete-btn">x</button>
+                    className="delete-btn">  <TiIcons.TiDelete/> </button>
         </li>
     )
 }
