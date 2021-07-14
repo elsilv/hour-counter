f348bda84ae97bc8cae0954440ea7402a872f191
elsilv <elsi.silvennoinen@gmail.com>
2021-07-14 12:27:32 +0300

    Added mongoose unique validator

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        unique: true,
         minlength: 4
     },
     name: {
@@ -15,6 +17,8 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }); 
+
+userSchema.plugin(uniqueValidator);
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
