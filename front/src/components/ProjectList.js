45aa32fb7578f072f07c04412d34d1df7908067e
elsilv <elsi.silvennoinen@gmail.com>
2021-08-06 10:47:42 +0300

    After adding a new project the project list refresh smoothly

--- front/src/components/ProjectList.js
+++ front/src/components/ProjectList.js
@@ -1,15 +1,20 @@
 import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { ProjectsUsed } from './ProjectsUsed.js';
 import { GlobalContext } from '../context/ProjectState';
 const jwt = require('jsonwebtoken');
 
 export const ProjectList = () => {
-    const { projects, getProjects } = useContext(GlobalContext);
+    const { projects, getProjects } = useContext(GlobalContext)
+    const history = useHistory()
 
     useEffect(() => {
         getProjects();
         // eslint-disable-next-line
-    }, []);
+        if (!localStorage.getItem("authToken")) {
+            history.push("/login")
+        }
+    }, [history, getProjects]);
 
     const authToken = localStorage.getItem("authToken")
     const userId = jwt.decode(authToken)
