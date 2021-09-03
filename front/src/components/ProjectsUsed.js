4a1cdcb80a65c6004162b9100ffad29bb7b40c48
elsilv <elsi.silvennoinen@gmail.com>
2021-09-03 14:11:51 +0300

    Styled edit project -window

--- front/src/components/ProjectsUsed.js
+++ front/src/components/ProjectsUsed.js
@@ -5,18 +5,6 @@ import ReactTooltip from 'react-tooltip';
 import * as AiIcons from 'react-icons/ai';
 import Modal from 'react-modal';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    color: '#000000',
-  },
-};
-
 export const ProjectsUsed = (props) => {
     const { deleteProject, changeCompleted, editProject } = useContext(GlobalContext);
 
@@ -33,7 +21,7 @@ export const ProjectsUsed = (props) => {
       console.log('edit')
 
       const newProject = {
-        name: name,
+        name: name
     }
 
       if (name.length > 2) { 
@@ -46,7 +34,7 @@ export const ProjectsUsed = (props) => {
       }
 
       function afterOpenModal() {
-        subtitle.style.color = '#283d3b';
+        subtitle.style.color = '#e6e8e6';
       }
     
       function closeModal() {
@@ -65,40 +53,58 @@ export const ProjectsUsed = (props) => {
                       <Link data-tip="On project page" className="project-list-btn" id="page-link" to={`/projects/${props.projects._id}`}><AiIcons.AiOutlineFolderOpen size={20}/></Link>
                     </u>
                     <u className="p2">
-                      <button data-tip="Edit" className="project-list-btn" >  <AiIcons.AiOutlineEdit size={20}/> </button>
-                    </u>
+                    <button data-tip="Edit" className="project-list-btn" onClick={openModal}> <AiIcons.AiOutlineEdit size={20}/> </button>
+                      <Modal
+                        isOpen={modalIsOpen}
+                        onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModal}
+                        ariaHideApp={false} 
+                        style={{
+                          overlay: {
+                            position: 'fixed',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            backgroundColor: 'rgba(-255, -255, -255, 0.35)'
+                          },
+                          content: {
+                            top: '50%',
+                            left: '50%',
+                            right: 'auto',
+                            bottom: 'auto',
+                            paddingBottom: '50px',
+                            marginRight: '-50%',
+                            transform: 'translate(-50%, -50%)',
+                            color: '#e6e8e6',
+                            background: '#324c4a',
+                            border: '2px solid #283d3b',
+                          },
+                        }}
+                        contentLabel="Modal">
+
+                          <button onClick={closeModal}> <AiIcons.AiOutlineClose/> </button>
+                          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Edit</h2>
+
+                          <form onSubmit={onSubmit}>
+                          <label htmlFor="text">Project name</label>
+                          <input id="project-name" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder={props.projects.name}/>
+                          
+                          <label htmlFor="amount">How much time do you want to spend?</label>
+                          <input id="project-amount" type="number" placeholder={props.projects.amount}/>
+
+                          <label htmlFor="description">Descripe your project</label>
+                          <input id="project-description" type="text-field" placeholder={props.projects.description} />
+                          <button> Save </button>
+                          </form>
+                      </Modal>
+                      </u>
                     <u className="p2">
                       <button className="project-list-btn" data-tip="Remove project" onClick={() => deleteProject(props.projects._id)}>  <AiIcons.AiOutlineDelete size={20}/></button>
                     </u>
                     <u className="p2">
                       <button data-tip="Mark project as completed" className="project-list-btn" onClick={() => changeCompleted(props.projects._id)}>  {label} </button>
                     </u>
-
-                    <button onClick={openModal}> <AiIcons.AiOutlineEdit size={20}/> </button>
-                      <Modal
-                        isOpen={modalIsOpen}
-                        onAfterOpen={afterOpenModal}
-                        onRequestClose={closeModal}
-                        ariaHideApp={false} 
-                        style={customStyles}
-                        contentLabel="Modal">
-
-                        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Edit</h2>
-                        <button onClick={closeModal}>close</button>
-
-                        <form onSubmit={onSubmit}>
-                        <label htmlFor="text">Project name</label>
-                        <input id="project-name" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder={props.projects.name}/>
-                        
-                        <label htmlFor="amount">How much time do you want to spend?</label>
-                        <input id="project-amount" type="number" placeholder={props.projects.amount}/>
-
-                        <label htmlFor="description">Descripe your project</label>
-                        <input id="project-description" type="text-field" placeholder={props.projects.description} />
-                        <button>Edit</button>
-                        </form>
-
-                      </Modal>
                   </p> 
         </div>       
       </>
